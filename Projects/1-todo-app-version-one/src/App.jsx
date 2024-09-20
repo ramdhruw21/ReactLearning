f94aa76a60be5f5dd2b0ecbba62ce6dc83b09d90
Ramji Dhruw <ramdhruw7482@gmail.com>
2024-09-20 19:18:40 +0530

    initial commit

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -1,11 +1,26 @@
-import AddTodo from "./Components/AddTodo";
 import AppName from "./Components/AppName";
+import "./Components/App.css";
+
 
 function App() {
   return (
     <center class="todo-container">
       <AppName></AppName>
-      
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-4">
+            <input type="text" placeholder="Enter TODO"/>
+          </div>
+          <div class="col-4">
+            <input type="date"/>
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+       </div>
+      </div>
+
         <div class="row">
           <div class="col-4">
              Buy Milk 
